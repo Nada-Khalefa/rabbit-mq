686438e5ccd76f6ccc140fa55fd7a5b84cb1dacf
nada khalefa <NadaKhalefa721@gmail.com>
2025-04-20 17:00:05 +0200

    Retry

--- src/main/java/com/spring/rabbit_mq/config/RabbitMqConfig.java
+++ src/main/java/com/spring/rabbit_mq/config/RabbitMqConfig.java
@@ -2,10 +2,12 @@ package com.spring.rabbit_mq.config;
 
 //import com.rabbitmq.client.ConnectionFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.AbstractConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -55,7 +57,11 @@ public class RabbitMqConfig {
         containerFactory.setPrefetchCount(5); // how many messages that consumer can fetch from queue
         containerFactory.setAutoStartup(true); // Listener work in initialization App
 
-        containerFactory.setDefaultRequeueRejected(false); // if error happen donot do reconsum in queue
+        //containerFactory.setDefaultRequeueRejected(false); // if error happen donot do reconsume in queue
+
+        // Retry only 3 times and take message to Q4
+        containerFactory.setAdviceChain(RetryInterceptorBuilder.stateless()
+                .maxAttempts(3).recoverer(new RejectAndDontRequeueRecoverer()).build());
 
         return containerFactory;
     }
